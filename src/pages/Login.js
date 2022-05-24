f03a2172c7a5c0df37ff88fc925bb64b9c532f3f
Yuri Villanueva <yuri.350@hotmail.com>
2022-05-24 17:57:58 -0300

    Requisito 4 inicio

---
+++ src/pages/Login.js
@@ -0,0 +1,15 @@
+import React from 'react';
+// import PropTypes from 'prop-types';
+// import { connect } from 'react-redux';
+
+class Login extends React.Component {
+  constructor() {
+    super();
+
+  }
+
+  render (
+    return (
+      
+    )
+  )
