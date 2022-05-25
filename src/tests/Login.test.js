79c40d740ccb35e96198611ab9472f361060538f
Yuri Villanueva <yuri.350@hotmail.com>
2022-05-25 17:05:59 -0300

    Requisito 4 criando rotas de teste

--- src/tests/Login.test.js
+++ src/tests/Login.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import App from '../App';
+import { createMemoryHistory } from 'history';
 import renderWithRouter from './helpers/renderWithRouterAndRedux';
 
 describe('1 - App.js', () => {
@@ -38,5 +39,62 @@ describe('1 - App.js', () => {
 
     userEvent.click(btnEl);
     expect(btnEl).toBeDisabled();
+  });
+
+  it('Input name ter o valor zerado e um userEvent com o nome Yuri', () => {
+    renderWithRouter(<App />);
+
+    const inputEl = screen.getByTestId('input-player-name');
+    expect(inputEl).toHaveValue('');
+
+    userEvent.type(inputEl, 'Yuri');
+    expect(inputEl).toHaveValue('Yuri');
+  });
+
+  it('Input name ter o valor zerado e um userEvent com o nome Yuri', () => {
+    renderWithRouter(<App />);
+
+    const inputEl = screen.getByTestId('input-gravatar-email');
+    expect(inputEl).toHaveValue('');
+
+    userEvent.type(inputEl, 'yuri.350@hotmail.com');
+    expect(inputEl).toHaveValue('yuri.350@hotmail.com');
+  });
+
+  it('Testa se ao preencher tudo corretamente o botao e liberado', () => {
+    renderWithRouter(<App />);
+
+    const inputEl = screen.getByTestId('input-gravatar-email');
+    expect(inputEl).toHaveValue('');
+
+    userEvent.type(inputEl, 'yuri.350@hotmail.com');
+    expect(inputEl).toHaveValue('yuri.350@hotmail.com');
+
+    const inputEl1 = screen.getByTestId('input-player-name');
+    expect(inputEl1).toHaveValue('');
+
+    userEvent.type(inputEl1, 'Yuri');
+    expect(inputEl1).toHaveValue('Yuri');
+
+    const btnEl = screen.getByTestId('btn-play');
+    expect(btnEl).toBeDefined();
+
+    userEvent.click(btnEl);
+    expect(btnEl).not.toBeDisabled();
+  });
+
+  it('Teste redirecionamento da pagina login para a proxima pagina', () => {
+    const customHistory = createMemoryHistory();
+    render(
+      <Router history={ customHistory }>
+        <App />
+      </Router>,
+    );
+
+    customHistory.push('/game');
+    const notFoundHeadingEl = screen.getByRole('heading', { leve: 2,
+      name: /Page requested not found/i });
+
+    expect(notFoundHeadingEl).toBeDefined();
   });
 });
