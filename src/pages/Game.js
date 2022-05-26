7b4d379426713417f5e12f6a1af6cf6c10cefb71
Thiago Fonseca de Souza <t.fs.2007@hotmail.com>
2022-05-26 16:39:10 -0300

    requisitos 9 e 11

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { fetchQuestionsAPI } from '../services/getAPI';
 import Header from '../components/Header';
+import { setScore } from '../redux/action';
 import './CSS/Game.css';
 
 class Game extends Component {
@@ -13,12 +15,14 @@ class Game extends Component {
       alternatives: [''],
       isDisabled: false,
       seconds: 30,
+      score: 0,
+      difficulty: '',
     };
   }
 
   async componentDidMount() {
-    const token = localStorage.getItem('token');
-    const getToken = await fetchQuestionsAPI(token);
+    const token = JSON.parse(localStorage.getItem('token'));
+    const getToken = await fetchQuestionsAPI(token.token);
     const { history } = this.props;
     const TOKEN_INVALID = 3;
     if (getToken.response_code === TOKEN_INVALID) {
@@ -55,21 +59,49 @@ class Game extends Component {
     const falseAlternatives = questions[indexQuestion + number].incorrect_answers;
     const correct = questions[indexQuestion + number].correct_answer;
     const alternatives = [correct, ...falseAlternatives];
+    const difficult = questions[indexQuestion + number].difficulty;
     // encontrei essa forma de embaralhar os itens do array no link abaixo.
     // https://www.delftstack.com/pt/howto/javascript/shuffle-array-javascript/#:~:text=utilizando%20Console.log()%3A-,function%20shuffleArray(inputArray)%7B%0A%20%20%20%20inputArray.sort(()%3D%3E%20Math.random()%20%2D%200.5)%3B%0A%7D,-var%20demoArray%20%3D%20%5B1
     const PARAMETER = 0.5;
     const newArr = alternatives.sort(() => Math.random() - PARAMETER);
     this.setState({
       alternatives: [...newArr],
+      difficulty: difficult,
     });
   }
 
-  handleClick = () => {
+  calculateDifficulty = () => {
+    const { difficulty } = this.state;
+    const POINT = 3;
+    switch (difficulty) {
+    case 'easy':
+      return 1;
+    case 'medium':
+      return 2;
+    case 'hard':
+      return POINT;
+    default:
+      return 0;
+    }
+  }
+
+  handleClick = (result) => {
+    const POINTS = 10;
+    const { seconds } = this.state;
     this.setState({
       isDisabled: true,
     });
     clearInterval(this.interval);
     clearTimeout(this.timeOut);
+    if (result === 'correct') {
+      this.setState((prev) => ({
+        score: prev.score + POINTS + (seconds * this.calculateDifficulty()),
+      }), () => {
+        const { setScoree } = this.props;
+        const { score } = this.state;
+        setScoree(score);
+      });
+    }
   }
 
   buttonNext = () => {
@@ -119,7 +151,7 @@ class Game extends Component {
                     className={ isDisabled === true ? 'correct' : '' }
                     key={ i }
                     disabled={ isDisabled }
-                    onClick={ this.handleClick }
+                    onClick={ () => this.handleClick('correct') }
                     data-testid="correct-answer"
                   >
                     {alternative}
@@ -129,7 +161,7 @@ class Game extends Component {
                   <button
                     type="button"
                     className={ isDisabled === true ? 'wrong' : '' }
-                    onClick={ this.handleClick }
+                    onClick={ () => this.handleClick('wrong') }
                     disabled={ isDisabled }
                     key={ i }
                     data-testid={ `wrong-answer-${i}` }
@@ -156,10 +188,15 @@ class Game extends Component {
   }
 }
 
+const mapDispatchToProps = (dispatch) => ({
+  setScoree: (score) => dispatch(setScore(score)),
+});
+
 Game.propTypes = {
   history: PropTypes.shape({
     push: PropTypes.func.isRequired,
   }).isRequired,
+  setScoree: PropTypes.func.isRequired,
 };
 
-export default Game;
+export default connect(null, mapDispatchToProps)(Game);
